259930561f778b0088bea3af55d78c0f3d60940f
kaus98 <kaus.pathak@gmail.com>
2022-01-15 03:55:35 +0530

    Updated Graphs

--- README.md
+++ README.md
@@ -56,3 +56,33 @@ We will start with building very simple Spiral graph in python which will act as
 * We will add the Y Ticks for starting of each year.
 * We will also add Watermark to add your name on bottom right.
 * Offset is added to not start the graph from zero.
+``` python
+def get_graph_base(max_n: int, 
+                   line_color: str, 
+                   offset_radius: int = 100,
+                   ancor_text: str = "By: kaus98"):
+    months = pd.Series([31, 28.25, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]).cumsum() #Calendar to set angles for each month
+    angles = pd.Series(np.linspace(0, 2*np.pi*(max_n/365) , max_n))
+    radius = pd.Series(list(range(max_n))) + offset_radius
+    
+    fig = plt.figure(figsize=(14,18)) 
+    ax = fig.add_subplot(111, polar=True)
+    
+    ax.set_theta_direction(-1) # Make Graph go Clockwise 
+    ax.set_theta_offset(np.pi/2.0) # Add Offset of 90 Deg
+    ax.set_xticks(np.deg2rad(months/months.max()*360).values) 
+    ax.set_xticklabels([ 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec','Jan']) # Setting the Labels for X Ticks
+    ax.set_yticks([0+offset_radius,365+offset_radius, 2*365+offset_radius]) # Adding the Y-Ticks for Each Year
+    ax.set_yticklabels(["", "", ""])    
+    ax.grid(True, color = "#000000", linewidth = "0.1", linestyle = "solid" )
+    
+    # Making the Base Spiral Line 
+    ax.plot(angles, radius, "--", color = line_color, linewidth = "1.2", label = "Days Since 2020/01/01")
+    ax.legend(loc = "upper right")
+    ancor_text = AnchoredText(ancor_text, loc = "lower right", prop=dict(alpha=0.4)) # Adding the Watermark Text
+    ax.add_artist(ancor_text)
+    
+    ax.set_title("COVID-19 Cases (11/01/2022)", y = 1.05, fontdict={"fontsize":26, "fontname": "impact"}) # Setting the title
+
+    return fig, ax, angles, radius
+```
--- core/graphs.py
+++ core/graphs.py
@@ -11,31 +11,29 @@ from matplotlib.offsetbox import AnchoredText
 def get_graph_base(max_n: int, 
                    line_color: str, 
                    offset_radius: int = 100,
-                   ancor_text: str = "By: Kaustubh Pathak"):
-    months = pd.Series([31, 28.25, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]).cumsum()
+                   ancor_text: str = "By: kaus98"):
+    months = pd.Series([31, 28.25, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]).cumsum() #Calendar to set angles for each month
     angles = pd.Series(np.linspace(0, 2*np.pi*(max_n/365) , max_n))
+    radius = pd.Series(list(range(max_n))) + offset_radius
     
     fig = plt.figure(figsize=(14,18)) 
     ax = fig.add_subplot(111, polar=True)
-    ax.set_theta_direction(-1)
-    ax.set_theta_offset(np.pi/2.0)
-    ax.set_xticks(np.deg2rad(months/months.max()*360).values)
-    ax.set_xticklabels([ 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec','Jan'])
-    ax.set_yticks([0+offset_radius,365+offset_radius, 2*365+offset_radius])
-    ax.set_yticklabels(["", "", ""])
     
+    ax.set_theta_direction(-1) # Make Graph go Clockwise 
+    ax.set_theta_offset(np.pi/2.0) # Add Offset of 90 Deg
+    ax.set_xticks(np.deg2rad(months/months.max()*360).values) 
+    ax.set_xticklabels([ 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec','Jan']) # Setting the Labels for X Ticks
+    ax.set_yticks([0+offset_radius,365+offset_radius, 2*365+offset_radius]) # Adding the Y-Ticks for Each Year
+    ax.set_yticklabels(["", "", ""])    
     ax.grid(True, color = "#000000", linewidth = "0.1", linestyle = "solid" )
     
-    radius = pd.Series(list(range(max_n))) + offset_radius
-    
-    # radius = [i+offset_radius for i in range(max_n)]
-    
+    # Making the Base Spiral Line 
     ax.plot(angles, radius, "--", color = line_color, linewidth = "1.2", label = "Days Since 2020/01/01")
     ax.legend(loc = "upper right")
-    ancor_text = AnchoredText(ancor_text, loc = "lower right", prop=dict(alpha=0.4))
+    ancor_text = AnchoredText(ancor_text, loc = "lower right", prop=dict(alpha=0.4)) # Adding the Watermark Text
     ax.add_artist(ancor_text)
     
-    ax.set_title("COVID-19 Cases (11/01/2022)", y = 1.05, fontdict={"fontsize":26, "fontname": "impact"})
+    ax.set_title("COVID-19 Cases (11/01/2022)", y = 1.05, fontdict={"fontsize":26, "fontname": "impact"}) # Setting the Title
     return fig, ax, angles, radius
 
 def spiral_graph_with_fill(dfs: list,
